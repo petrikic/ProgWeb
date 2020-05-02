8b88836732b77532019116297e743ec0e9305686
Cleriston Nantes Petrikic <arteefx2012@gmail.com>
2020-05-02 13:54:00 -0400

    Adicionado a atividade página pessoal

---
+++ pagina-pessoal/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cleriston Nantes Petrikic - Perfil</title>
+  <link rel="stylesheet" href="estilos.css">
+  <link rel="icon" type="imagem/png" href="foto.png" />
+</head>
+
+<body>
+  <div class="conteudo">
+    <div class="foto"><img src="foto.png" alt=""></div>
+    <div class="apresentacao">
+      <span class="nome">Cleriston Nantes Petrikic</span>
+      <span class="curso">Tecnólogo em Análise e Desenvolvimento de Sistemas - FACOM UFMS</span>
+      <span class="email">cleriston.nantes@ufms.br</span>
+      <a href="http://github.com/petrikic/" target="_blank"><span class="github">github.com/petrikic/</span></a>
+    </div>
+  </div>
+  <footer>
+
+    <?php 
+
+      date_default_timezone_set('America/Campo_Grande');  
+      Echo "Página gerada em " .date('d/m/Y \à\s H:i:s');
+    
+    ?>
+
+  </footer>
+</body>
+
+</html>
